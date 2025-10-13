7b7dd39d6ab128551d920edfc18c14cde3d50b01
Hamza <153912992+MuhammadHamzaAslam@users.noreply.github.com>
2025-10-13 22:54:46 +0500

    Login Api With JWT Token Generation added

--- Backend/src/lib/utility.js
+++ Backend/src/lib/utility.js
@@ -1,4 +1,6 @@
 import bcrypt from "bcrypt";
+import env from "../constant/env.js";
+import jwt from "jsonwebtoken";
 
 // Function to convert plain text password to hashed password
 export const covertPasswordToHash = (password) => {
@@ -10,4 +12,25 @@ export const covertPasswordToHash = (password) => {
 // Generate Random OTP
 export const generateOTP = () => {
   return Math.floor(100000 + Math.random() * 900000);
+};
+
+// Function to compare plain text password with hashed password
+export const comparePassword = async (password, hashedPassword) => {
+  return await bcrypt.compare(password, hashedPassword);
+};
+
+const generateToken = (payload, expiresIn = "7d") => {
+  return jwt.sign(payload, env.JWT_SECRET, { expiresIn: expiresIn });
+};
+
+export const createAuthToken = (user, expiresIn = "7d") => {
+  return generateToken(
+    {
+      id: user._id,
+      email: user.email,
+      user_name: user.user_name,
+      password: user.password,
+    },
+    expiresIn
+  );
 };
--- Backend/src/routes/auth/auth.route.js
+++ Backend/src/routes/auth/auth.route.js
@@ -2,7 +2,12 @@ import express from "express";
 import { StatusCodes } from "http-status-codes";
 import { z } from "zod";
 import User from "../../models/User.js";
-import { covertPasswordToHash, generateOTP } from "../../lib/utility.js";
+import {
+  comparePassword,
+  covertPasswordToHash,
+  createAuthToken,
+  generateOTP,
+} from "../../lib/utility.js";
 import dayjs from "dayjs";
 import { sendEmailOTP } from "../../constant/email.js";
 
@@ -10,6 +15,11 @@ const authRouter = express.Router();
 
 const SignupSchema = z.object({
   user_name: z.string().min(3).max(30),
+  email: z.string().email(),
+  password: z.string().min(6).max(100),
+});
+
+const LoginSchema = z.object({
   email: z.string().email(),
   password: z.string().min(6).max(100),
 });
@@ -73,6 +83,61 @@ authRouter.post("/signup", async (req, res) => {
     });
   }
 });
-authRouter.post("/login", (req, res) => {});
+authRouter.post("/login", async (req, res) => {
+  const { data, success, error } = LoginSchema.safeParse(req.body);
+  if (!success) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      success: false,
+      error: true,
+      message: "All Fields are required",
+      details: error.errors,
+    });
+  }
+  try {
+    const user = await User.findOne({ email: data.email });
+
+    if (!user) {
+      return res.status(StatusCodes.NOT_FOUND).json({
+        success: false,
+        error: true,
+        message: "User not found with this email",
+      });
+    }
+
+    // if (!user.isEmailVerified) {
+    //   return res.status(StatusCodes.UNAUTHORIZED).json({
+    //     success: false,
+    //     error: true,
+    //     message: "Email is not verified. Please verify your email.",
+    //   });
+    // }
+
+    const isPasswordCorrect = comparePassword(data.password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        success: false,
+        error: true,
+        message: "Invalid Password! Kindly enter correct password.",
+      });
+    }
+    return res.status(StatusCodes.OK).json({
+      success: true,
+      error: false,
+      message: "User Login Successfully.",
+      data: {
+        user,
+        token: createAuthToken(user),
+      },
+    });
+  } catch (error) {
+    console.log("error ==>", error);
+
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      error: true,
+      message: "Error in logging in. Please try again.",
+    });
+  }
+});
 
 export default authRouter;
